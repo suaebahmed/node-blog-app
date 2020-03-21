968e5abbd9f60cd964026a43f16a661f77dc079c
suaebahmed <suaebahmed12@gmail.com>
2020-03-21 21:42:05 +0600

    complate update route

--- routes/blog-route.js
+++ routes/blog-route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Post = require('../models/blog-models')
 
-router.get('/',(req,res,next)=>{
+router.get('/',(req,res,next)=>{  // render index page with all post
     Post.find((err,allpost)=>{
         if(err)
             res.send('err, somthing went wrong.')
@@ -11,11 +11,10 @@ router.get('/',(req,res,next)=>{
     })
 })
 
-router.get('/post',(req,res)=>{
+router.get('/post',(req,res)=>{   // render createPost page
   res.render('createPost')
 })
 router.post('/create',(req,res,next)=>{
-    console.log(req.body)
     var newPost = new Post({
         title: req.body.title,
         body: req.body.body,
@@ -31,7 +30,7 @@ router.post('/create',(req,res,next)=>{
     })
 })
 
-router.get('/post/:id',(req,res,next)=>{
+router.get('/post/:id',(req,res,next)=>{ // render indevisual page
     const id = req.params.id;
     Post.findById(id,(err,post)=>{
         if(err){
@@ -42,7 +41,7 @@ router.get('/post/:id',(req,res,next)=>{
     })
 })
 
-router.delete('/delete/:id',(req,res,next)=>{
+router.get('/delete/:id',(req,res,next)=>{
     const id = req.params.id;
     Post.deleteOne({_id: id},(err)=>{
         if(err){
@@ -54,31 +53,55 @@ router.delete('/delete/:id',(req,res,next)=>{
     })
 })
 
-router.get('/editPost',(req,res,next)=>{
-    res.render('editPost');
-})
-router.patch('/update/:id',(req,res,next)=>{
+router.get('/editPost/:id',(req,res,next)=>{ // render editPost page
     const id = req.params.id;
-    console.log(id)
-    Post.findOne({_id: id},(err,post)=>{
+    Post.findById(id,(err,post)=>{
         if(err){
+            res.type('html')
             res.status(500);
             res.send('Error',err)
             res.end()
         }else{
-            console.log(post)
-            var newPost = new Post({
+            res.render('editPost',{post})
+        }    
+    })
+})
+router.post('/update/:id',(req,res,next)=>{ // must have post method
+    const id = req.params.id;
+    Post.findOne({_id: id},(err,post)=>{
+        if(err){
+            res.type('html')
+            res.status(500);
+            res.send('Error',err)
+            res.end()
+        }else{
+            var newPost ={
                 title: req.body.title,
                 body: req.body.body,
                 author: req.body.author
-            })
-            post = newPost
+            }
+
+            post.title = newPost.title;
+            post.body = newPost.body;
+            post.author = newPost.author;
+            // console.log(newPost)
+
+// ----------------- they create new Post---------------
+            // var s = new Post({ 
+            //     title: req.body.title,
+            //     body: req.body.body,
+            //     author: req.body.author                
+            // })
+            // s.save((err)=>{
+
             post.save((err)=>{
                 if(err){
-                    res.status(500);
-                    res.send('Error',err)
+                    res.type('html')
+                    res.status(500);        
+                    res.send('Error')
                     res.end()
-                }else{
+                    }
+                else{
                     res.redirect('/')
                 }
             })       
